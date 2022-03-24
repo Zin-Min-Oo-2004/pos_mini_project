9f8fd88f3fa79b8d763702c10756e154089d9dba
Zin-Min-Oo-2004 <zinminoo.developer@gmail.com>
2022-03-24 12:09:46 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="row">
+        <div className="col-1 border-right p-0">
+          <h1>First Part</h1>
+        </div>
+        <div className="col-2 border-right product_background_color">
+          <div className="product_list_row">
+            <div className="width-10 single_product">
+              <h3>Title a san htae kyi tar pr kha bya</h3>
+              <p>1000 MMK</p>
+              <button className="add_to_cart">Add To Cart</button>
+            </div>
+            <div className="width-10 single_product">
+              <h3>Title</h3>
+              <p>1000 MMK</p>
+              <button className="add_to_cart">Add To Cart</button>
+            </div>
+            <div className="width-10 single_product">
+              <h3>Title</h3>
+              <p>1000 MMK</p>
+              <button className="add_to_cart">Add To Cart</button>
+            </div>
+            <div className="width-10 single_product">
+              <h3>Title</h3>
+              <p>1000 MMK</p>
+              <button className="add_to_cart">Add To Cart</button>
+            </div>
+            <div className="width-10 single_product">
+              <h3>Title</h3>
+              <p>1000 MMK</p>
+              <button className="add_to_cart">Add To Cart</button>
+            </div>
+            <div className="width-10 single_product">
+              <h3>Title</h3>
+              <p>1000 MMK</p>
+              <button className="add_to_cart">Add To Cart</button>
+            </div>
+            <div className="width-10 single_product">
+              <h3>Title</h3>
+              <p>1000 MMK</p>
+              <button className="add_to_cart">Add To Cart</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-1">
+          <h1>Third Part</h1>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
